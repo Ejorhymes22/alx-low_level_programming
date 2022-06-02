4871dbf37d6cb918a05e973f9061ced97dde57b1
Lukman Mohammed <ejorhymes22@gmail.com>
2022-06-02 10:15:56 -0700

    first 8 tasks on c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints with printf
+ *
+ * Return: success (0)
+ */
+
+int main(void)
+{
+	char s[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", s);
+	return (0);
+}
