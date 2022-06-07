6052dc4d82ac062092f49300116f3ec0a101467d
Lukman Mohammed <ejorhymes22@gmail.com>
2022-06-07 10:01:29 -0700

    lines adjusted

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,13 +8,9 @@
 
 void print_times_table(int n)
 {
-	int i = 0;
-       	int j = 0; 
-	int x;
+	int i, j, k;
 
-	if (n < 0 || n > 15)
-		;
-	else
+	if (n > 0 && n < 15)
 	{
 		for (; i <= n; i++)
 		{
