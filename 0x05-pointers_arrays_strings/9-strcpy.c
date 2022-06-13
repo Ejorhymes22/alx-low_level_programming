35171ac60ebcb241dd61f61e11af5c352fde843b
Lukman Mohammed <ejorhymes22@gmail.com>
2022-06-13 10:31:16 -0700

    betty cleared

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src including the 
+ * _strcpy - copies the string pointed to by src including the
  * terminating null byte to the buffer pointed to by dest.
  * @dest: pointer to where the copy lies
  * @src: pointer to source
@@ -17,4 +17,4 @@ char *_strcpy(char *dest, char *src)
 		dest[i++] = *src++;
 	dest[i] = '\0';
 	return (dest);
-}	
+}
