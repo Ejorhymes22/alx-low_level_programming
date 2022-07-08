9578e1a7e9e587cc9b8c6ada1677cc3a0d47e661
Lukman Mohammed <ejorhymes22@gmail.com>
2022-07-08 03:58:16 -0700

    case switch addd

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ * @format: specifiers
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	int j = 0;
+	va_list list;
+	char *p;
+
+	while (format[j])
+		j++;
+
+	va_start(list, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;	
+			case 's':
+				p = va_arg(list, char *);
+				if (!p)
+					printf("(nil)");
+				else
+					printf("%s", p);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+		if (i < j)
+			printf(", ");
+	}
+	printf("\n");
+	va_end(list);
+}
