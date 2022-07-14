57cca7ac0e1f14463acc0d34707c9e4a5f1f653c
Lukman Mohammed <Ejorhymes22@gmail.com>
2022-07-14 12:47:57 -0700

    gaurd added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,3 +24,5 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void print_first(void) __attribute__ ((constructor));
 size_t print_list(const list_t *h);
+
+#endif
