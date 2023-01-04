20a2dc2d7c83d8a4614c65ca6bb6c2b113dbbe3e
Lukmnan Mohammed <ejorhymes22@gmail.com>
2023-01-04 08:19:03 -0800

    a

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,6 +15,8 @@ int binarySearch(int arr[], size_t l, size_t r, int x);
 
 int binary_search(int *array, size_t size, int value)
 {
+	if (!array)
+		return (-1);
 	return (binarySearch(array, 0, size - 1, value));
 }
 
