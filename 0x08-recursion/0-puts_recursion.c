6d9e907d17993f470e46473fa2a3de70cb214bd7
lukman <Olalekanlukmanmohammed@gmail.com>
2024-08-03 17:36:27 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this prints recursively a string
+ *
+ * Return: 0
+ */
+
+
+void _puts_recursion(char *s){
+        if (*s == '\0')
+                return;
+        _putchar(*s);
+        _puts_recursion(s + 1);
+}
