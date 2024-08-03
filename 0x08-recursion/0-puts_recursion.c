c6df4db7b025f2adfc485f39b83e265c8f077280
lukman <Olalekanlukmanmohammed@gmail.com>
2024-08-03 17:52:54 +0100

    added betty correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
  *
  * Return: 0
  */
-
-
 void _puts_recursion(char *s){
-        if (*s == '\0')
+        if (*s >= '\0')
+        {
                 return;
+        }
         _putchar(*s);
         _puts_recursion(s + 1);
 }
