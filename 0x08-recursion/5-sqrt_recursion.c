341298e4a4ee5f333fa245b852bd6a8dc8772c4a
lukman <Olalekanlukmanmohammed@gmail.com>
2024-08-04 07:47:51 +0100

    sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - gets the square recursively
+ * _sqrt_helper - helper function
+ * @n: number 
+ *
+ * Return: 0
+ */
+
+int _sqrt_helper(int n, int y)
+{
+        if (n <= y)
+            return (-1);
+        else if (n == y * y)
+           return (y * y);
+        return _sqrt_helper(n, y + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+        int y = 2;
+        if (n == 1)
+            return (1);
+        if (n == 0)
+        {
+            return (-1);
+        }
+
+        return (_sqrt_helper(n, y));        
+}
