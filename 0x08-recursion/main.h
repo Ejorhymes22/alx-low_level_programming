ff3fd644d15accc5b292f72c6f8c369ae978adbf
lukman <Olalekanlukmanmohammed@gmail.com>
2024-08-04 09:56:25 +0100

    is_palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,8 @@ int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int y);
 int is_prime_number(int n);
 int _prime_helper(int n, int y, int sum);
+int is_palindrome(char *s);
+int palindromeChecker(char *str, int len, int i);
+int _strlen_recursion(char *s);
+ int is_palindrome(char *s);
+
